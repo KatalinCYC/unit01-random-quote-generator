958c4eaa4f6347a8af672e333a733e4724be44d3
KatalinCYC <katalin.clarijs@youngcoders.nl>
2021-09-02 14:56:41 +0200

    Added changes

--- js/script.js
+++ js/script.js
@@ -11,9 +11,8 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
-// Here are the quotes I chose. I added a tag that says what sort of medium the quote is from. 
 
-const quotes = [
+const quotes = [ // Here are the quotes that will pop up on screen. They contain the quotes themselves, the person who said those quotes, in what year they were said, where they were said and what sort of medium they came from.
   {quote: "Ideologies are like arseholes: everyone's got one, we use it every day, but you very rarely look at your own unless something's gone wrong.", source: "Abigail Thorn", year: "2021", citation: "Jordan Peterson's Ideology", tag: "video essay"},
   {quote: "If you'd had told me 400 years ago that one day, I'd be watching my decapitated body having an affair with a 60-year old dead woman, I'd have said you were mad.", source: "Humphrey Bone (Laurence Richard)", year: "2021", citation: "Ghosts", tag: "sitcom"},
   {quote: "You look so sweet.", source: "Jan-Peter Balkenende, Dutch Prime Minister (2002-2010)", year: "2010", citation: "RTL Election Debate", tag: "political"},
@@ -25,9 +24,8 @@ const quotes = [
  * `getRandomQuote` function
 ***/
 
-// The following function will pick a random quote from the array.
 
-function getRandomQuote() {
+function getRandomQuote() { // In this function a random quote is picked, by first selecting a random number and then returning the corresponding quote from the array.
 let quoteNumber = Math.floor(Math.random() * quotes.length);  
 return quotes[quoteNumber];
 }
@@ -37,9 +35,8 @@ return quotes[quoteNumber];
  * `printQuote` function
 ***/
 
-// This function will show the quote with underneath it the source, citation and year. In the bottom right corner you'll see the tag, which was made grey so it wouldn't stand out too much.
 
-function printQuote(quotes){
+function printQuote(quotes){ // In this function, I didn't change the lay-out of the original quote. I did add an extra class called "tag", which will be shown on the bottom right in a grey color, so it won't stand out too much.
 let randomQuote = getRandomQuote();
 let html = `<p class='quote'>${randomQuote.quote}</p><p class='source'>${randomQuote.source}<span class='citation'>${randomQuote.citation}</span><span class='year'>${randomQuote.year}</span></p>
 <p class='tag'>${randomQuote.tag}</p>`;
